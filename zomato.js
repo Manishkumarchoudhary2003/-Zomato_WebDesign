69e68e1069834a841d52761d78a60302b8f851ef
Manish Kumar Choudhary <107823673+Manishkumarchoudhary2003@users.noreply.github.com>
2023-03-28 00:48:39 +0530

    Add files via upload

---
+++ zomato.js
@@ -0,0 +1,18 @@
+function aboutjodhpur()
+{
+    alert("Welcome to Jodhpur")
+    alert("Order food online in Jodhpur, food delivery in Jodhpur - Zomato.")
+}
+function Trending()
+{
+    alert("Trending food in Jodhpur are- ")
+    alert("1. The Makhaniya Lassi 2. Dal Bati Churma 3. Dahi Chaat 4. Kadhi Kachhori /pyaaz kachori 5.Pani Patasa 6. Malai Ghevar 7. Mirchi Bada 8. Matka Kulfi.")
+}
+function sweet()
+{
+    alert("Made from pure milk and a generous sprinkling of pistachio, Gulab Halwa has made its place as one of the most popular desserts in Jodhpur.")
+}
+function raj()
+{
+    alert("From the sumptuous Rajasthani thali to curious dishes like gulab jamun ki sabzi and meaty delights like laal mas, Jodhpur is truly a food lovers' paradise. And then you have the famous kachoris and pani patasa, makhaniya lassi and sweet meats like ghevar and malpua to tickle your taste buds.")
+}
